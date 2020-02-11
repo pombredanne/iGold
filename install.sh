7e03cab22562d18bfa33069454175e2a02bed1d0
Michael Hyndman <42243876+SherlocksHat@users.noreply.github.com>
2020-02-11 22:28:40 +1030

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,15 @@
-
+!#/bin/bash
 
 #firebase testing
 sudo pip3 install pyrebase
 
-
-#bulk download (used on a Mac)
+#bulk download (to be installed on a Mac)
 npm install -g bagbak
 
-
-#file conversion
+#plist conversion
 sudo apt-get install libplist-utils
-git clone https://github.com/wireghoul/graudit ~/graudit
-ln -s ~/graudit/graudit ~/bin/graudit
 
+#class-dumps on kali
 wget https://raw.githubusercontent.com/MobSF/Mobile-Security-Framework-MobSF/blob/master/StaticAnalyzer/tools/mac/class-dump
 wget https://raw.githubusercontent.com/MobSF/Mobile-Security-Framework-MobSF/blob/master/StaticAnalyzer/tools/mac/class-dump-swift
 wget https://raw.githubusercontent.com/MobSF/
