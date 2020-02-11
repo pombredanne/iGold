8db94c8d292e4a72ba7c975b47b8cbbb62a30fc3
Michael Hyndman <42243876+SherlocksHat@users.noreply.github.com>
2020-02-11 22:38:57 +1030

    Update 2-iOS-bulk-conversion.sh

--- scripts/2-iOS-bulk-conversion.sh
+++ scripts/2-iOS-bulk-conversion.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
-#requirements - class-dump
-
 fontbold=$(tput bold)
 fontnormal=$(tput sgr0)
 tn=8
 dc=1
 
-echo "    _  ___  ____   ____                     _ _ _   "
-echo "   (_)/ _ \/ ___| / ___|_ __ __ _ _   _  __| (_) |_ "
-echo "   | | | | \___ \| |  _| '__/ _\` | | | |/ _\` | | __|"
-echo "   | | |_| |___) | |_| | | | (_| | |_| | (_| | | |_ "
-echo "   |_|\___/|____/ \____|_|  \__,_|\__,_|\__,_|_|\__|"
-echo "                                                    "
-echo "   iOS APP AUDIT TOOL "
-
-echo -e "\n   Welcome to the iOS app code review tool"
-echo -e "   After using a tool like Bagbak .ipa extraction to decrypt and extract an iOS app,"
-echo -e "   the app folder can then be copied to your computer. iOSGraudit will then scan"
-echo -e "   nominated folder for any secrets or vulnerable code.\n"
-echo -e "   To make this even more effective, before running this tool, input any known"
-echo -e "   unique strings associated with the target (ie. cookie ID's) into the unique string"
-echo -e "   list 'signatures\uniquestrings.lst'\n"
-
 echo -e "\n    Start by typing the name of the folder you wish to scan.\n"
 read folder
+
 echo ""
 ######################################
 echo -e "   ${fontbold}[$dc/$tn]${fontnormal} Building working directories			STARTED"
 
 mkdir -p "$folder"-audit/{index,output,scan/{executable,binaries,plist,other}}
-
-
 
 echo -e "                                    			COMPLETED\n"
 ######################################
