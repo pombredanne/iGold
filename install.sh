ba2b781ba93044834cc068ddaa086dc32edd900f
Michael Hyndman <42243876+SherlocksHat@users.noreply.github.com>
2020-02-11 13:52:23 +1030

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+
+
+#firebase testing
+sudo pip3 install pyrebase
+
+
+#bulk download (used on a Mac)
+npm install -g bagbak
+
+
+#file conversion
+sudo apt-get install libplist-utils
+git clone https://github.com/wireghoul/graudit ~/graudit
+ln -s ~/graudit/graudit ~/bin/graudit
+
+wget https://raw.githubusercontent.com/MobSF/Mobile-Security-Framework-MobSF/blob/master/StaticAnalyzer/tools/mac/class-dump
+wget https://raw.githubusercontent.com/MobSF/Mobile-Security-Framework-MobSF/blob/master/StaticAnalyzer/tools/mac/class-dump-swift
+wget https://raw.githubusercontent.com/MobSF/
+
+#flash conversion
+sudo apt-get install swfmill
