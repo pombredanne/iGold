ab66046f181ce7b235df2f5d6b1e1dbbf160a06c
Michael Hyndman <42243876+SherlocksHat@users.noreply.github.com>
2020-02-12 21:41:26 +1030

    Update 7-list-ATS-domain-exceptions.sh

--- scripts/7-list-ATS-domain-exceptions.sh
+++ scripts/7-list-ATS-domain-exceptions.sh
@@ -1,3 +1,3 @@
 echo "enter the folder you wish to scan"
 read folder
-find "$folder" -name Info.plist -exec grep -o -P '(?<=key>).*(?=<)' {} \; | grep '\.'
+find "$folder" -name Info.plist -exec grep -o -P '(?<=key>).*(?=<)' {} \; | grep '\.' | sort -u
