63ddc07f64da65034633738035a2e84c8f39b495
Michael Hyndman <42243876+SherlocksHat@users.noreply.github.com>
2020-02-22 00:04:21 +1030

    Update 8-search-plist-files.sh

--- scripts/8-search-plist-files.sh
+++ scripts/8-search-plist-files.sh
@@ -1,3 +1,3 @@
-find <folder name> -name *.plist -exec grep -o -P '(?<=>).*(?=<)' {} \; | grep '(api|token|key)' -IE -A 10 -B 10 --color=always
+find <foldername> -name *.plist -exec grep -o -P '(?<=>).*(?=<)' {} \; | grep -i '(api|token|key|secret)' -IE -A 10 -B 10 --color=always
  ## find keys across all plists 'at scale'
 find <folder name> -name *.plist -exec grep -o -P '(?<=>).*(?=<)' {} \; ## read all plist files
